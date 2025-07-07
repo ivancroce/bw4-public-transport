4167945892795aa38de231279894e6d034320fea
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-07-07 09:12:23 +0100

    first commit

---
+++ src/main/java/ictech/Application.java
@@ -0,0 +1,12 @@
+package ictech;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bw4_public_transport_pu");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
